c5b952a45a65e3ca18158c6aa6b13d81a9c64955
xalanq <xalanq@gmail.com>
2024-09-13 16:52:08 +0800

    upd: add darwin nocgo compile support

--- cpu/cpu_darwin_nocgo.go
+++ cpu/cpu_darwin_nocgo.go
@@ -3,4 +3,15 @@
 
 package cpu
 
+import "errors"
+
 // CPU counters for darwin is unavailable without cgo.
+
+func Get() (*Stats, error) {
+	return nil, errors.New("cpu statistics not implemented for darwin nocgo")
+}
+
+// Stats represents cpu statistics
+type Stats struct {
+	User, System, Idle, Nice, Total uint64
+}
